b04c384cde71c421cf6f64e722de194f72752c24
alessandro natilla <alexander@localhost.localdomain>
2012-12-26 13:44:34 +0100

    added parameter <number of lines>

--- readlog.sh
+++ readlog.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 usage(){
-  echo "usage: $(basename $0) <file path>"
+  echo "usage: $(basename $0) <file path> <number of lines>"
 }
 
-if [[ $# -eq 0 ]]; then
+if [[ $# -eq 0 || $# -eq 1 ]]; then
   usage
   exit 1
 fi
@@ -12,19 +12,18 @@ fi
 																									    
 COUNTER=0
 TIMESTAMP=$(stat -c '%Z' $1)
-LINES=5
 
 while true; do
 	if [ $COUNTER -eq 0 ]; then
 		TIMESTAMP=$( stat -c '%Z' $1 )
 		let COUNTER++
-                tail -n $LINES $1
+                tail -n $2 $1
 		
 		elif [ $TIMESTAMP -ne $(stat -c '%Z' $1) ]; then
 			$TIMESTAMP = $(stat -c '%Z' $1)
 			echo -e "Updated at " $TIMESTAMP " :"
 			let COUNTER++
-	        	tail -n $LINES $1
+	        	tail -n $2 $1
 	fi
 	
 sleep 30s
